30f124060256a6eaf216238a6403de917eac7353
n9tank <n9tank@163.com>
2023-10-30 18:22:44 +0800

    0

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,15 @@ import rust.tmx.triggers;
 import rust.tmx.unitObjects;
 import rust.tmx.unitDetect;
 import rust.tmx.unitType;
+import java.io.BufferedWriter;
 public class Main {
  public static void main(String ...arg) throws Exception {
-  triggers all=new triggers(triggers.map_, new OutputStreamWriter(System.out), 0);
-  unitObjects obj=new unitObjects(all.mbuff);
-  unitDetect unit=new unitDetect(0, 0, 0, 0, all);
-  obj.add(unit,0);
-  all.finsh();
-  all.mbuff.close();
+ // triggers all=new triggers(triggers.map_, new OutputStreamWriter(System.out), 0);
+  BufferedWriter buff=new BufferedWriter(new OutputStreamWriter(System.out));
+  unitObjects obj=new unitObjects(buff);
+  obj.add(0, 0, 0, unitType.sea_gunBoat);
+  obj.end();
+  buff.flush();
   System.out.println();
  }
 }
