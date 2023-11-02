be4bb291ed47d3f4abf6e19231fc3375e7117b66
n9tank <n9tank@163.com>
2023-11-02 16:26:29 +0800

    0

--- src/rust/tmx/memory/Madder.java
+++ src/rust/tmx/memory/Madder.java
@@ -1,65 +1,37 @@
 package rust.tmx.memory;
 import java.util.HashMap;
-import rust.tmx.spawnUnit;
 import rust.tmx.triggers;
 import rust.tmx.unitAdd;
 import rust.tmx.unitDetect;
-import rust.tmx.unitRemove;
 import rust.tmx.unitType;
 
-public class Madder {
- private triggers m;
- private float x;
- private float y;
- private float w;
- private boolean safe;
- private String unit;
- private unitRemove rest;
+public class Madder extends MunitBox {
  private HashMap<Integer,unitAdd> map;
  private HashMap<Madder$key,unitDetect> eqz;
- private int team;
  public int max;
  public int addMax;
  public unitAdd[] adds;
  public unitDetect[] finds;
  protected byte findlink;
  public Madder(triggers trg) {
-  this(trg, trg.getPos(1f, 1f), trg.y, 1f, unitType.other_dummyNonUnitWithTeam,0);
+  this(trg.getPos(1f, 1f), trg.y, 1f, 0, unitType.other_dummyNonUnitWithTeam, trg);
   safe = true;
  }
- public Madder(triggers trg, float x0, float y0, float w0, String type, int t) {
-  m = trg;
-  x = x0;
-  y = y0;
-  w = w0;
-  unit = type;
+ public Madder(float x0, float y0, float w0, int t, String type, triggers trg) {
+  super(x0, y0, w0, w0, t, type, trg);
   map = new HashMap();
   eqz = new HashMap();
-  team = t;
  }
  public unitAdd add(int i) {
-  if(i<=max&&adds!=null)throw new RuntimeException("add():addLink()");
+  if (i <= max && adds != null)throw new RuntimeException("add():addLink()");
   Integer obj= Integer.valueOf(i);
   HashMap<Integer, unitAdd> table=map;
   unitAdd add=table.get(obj);
   if (add == null) {
-   spawnUnit sp=new spawnUnit(unit, i);
-   float w0=w *0.5f;
-   add = new unitAdd(x + w0, y + w0, m, team, sp);
-   add.resetActivationAfter = "1s";
+   add = add(unit(i));
    table.put(obj, add);
   }
   return add;
- }
- public unitRemove rest() {
-  unitRemove re=rest;
-  if (re == null) {
-   float w0=w;
-   rest = re = new unitRemove(x, y, w0, w0, m);
-   re.resetActivationAfter = "1s";
-   if (!safe)re.team = team;
-  }
-  return re;
  }
  private static int binary(int to) {
   int c;
@@ -72,7 +44,7 @@ public class Madder {
   return c;
  }
  public unitAdd[] addLink(int i) {
-  if(map.size()>0)throw new RuntimeException("addLink():add()");
+  if (map.size() > 0)throw new RuntimeException("addLink():add()");
   unitAdd[] add=adds;
   int tmp=addMax;
   if (add == null) {
@@ -80,15 +52,8 @@ public class Madder {
    int fu=1 << c;
    add = new unitAdd[c];
    adds = add;
-   float w0=w*0.5f;
-   float x0=x+w0;
-   float y0=y+w0;
-   int te=team;
-   String type=unit;
-   triggers trg=m;
    while (--c >= 0) {
-    unitAdd addn = new unitAdd(x0, y0, trg, te, new spawnUnit(type, fu));
-    addn.resetActivationAfter = "1s";
+    unitAdd addn =add(unit(i));
     add[c] = addn;
     fu >>= 1;
    }
@@ -107,8 +72,8 @@ public class Madder {
   return ru;
  }
  public unitDetect find(int i) {
-  if(findlink>0)throw new RuntimeException("find():initLink()");
-  findlink=-1;
+  if (findlink > 0)throw new RuntimeException("find():initLink()");
+  findlink = -1;
   unitDetect[] add=finds;
   if (add == null) {
    int tmp=max;
@@ -121,13 +86,13 @@ public class Madder {
   return add[i];
  }
  private int initLink() {
-  if(findlink<0)throw new RuntimeException("initLink():find()");
-  findlink=1;
+  if (findlink < 0)throw new RuntimeException("initLink():find()");
+  findlink = 1;
   unitDetect[] add=finds;
   int tmp=max << 1;
   int tmp3=tmp % 3;
   if (add == null) {
-   if(eqz.size()>0)throw new RuntimeException("initLink():of()");
+   if (eqz.size() > 0)throw new RuntimeException("initLink():of()");
    int tmp2 = tmp / 3 + tmp3 == 0 ?1: tmp3;
    add = new unitDetect[tmp2];
    finds = add;
@@ -150,7 +115,7 @@ public class Madder {
  private int getOf(int tmp3, int in) {
   int i = in + tmp3;
   tag: {
-   if (i < 0 || (w == 0 && tmp3 >= 0)) {
+   if (i < 0 || (i == 0 && tmp3 >= 0)) {
     i = 0;
     break tag;
    }
@@ -168,7 +133,7 @@ public class Madder {
    ru = add[i];
    int mx=ru.maxUnits;
    if (mx > 0 || (mx == 0 && tmp > 0)) {
-    i+=max-mx;
+    i += max - mx;
     ru = add[i];
    }
   } else ru = ofN(min, max);
@@ -200,20 +165,14 @@ public class Madder {
   return link;
  }
  private unitDetect add(int min, int ax) {
-  float w0=w;
-  unitDetect de=new unitDetect(x, y, w0, w0, m);
+  unitDetect de=detect();
   de.minUnits = min;
-  de.resetActivationAfter = "1s";
   if (ax < max)de.maxUnits = ax;
-  if (!safe) {
-   de.unitType = unit;
-   de.team = team;
-  }
   return de;
  }
- public unitDetect of(int min,int max){
-  if(finds!=null)throw new RuntimeException("of():initLink()");
-  return ofN(min,max);
+ public unitDetect of(int min, int max) {
+  if (finds != null)throw new RuntimeException("of():initLink()");
+  return ofN(min, max);
  }
  private unitDetect ofN(int min, int max) {
   Madder$key key=new Madder$key(min, max);
--- src/rust/tmx/memory/McheckBox.java
+++ src/rust/tmx/memory/McheckBox.java
@@ -8,84 +8,27 @@ import rust.tmx.unitObjects;
 import rust.tmx.unitRemove;
 import rust.tmx.unitType;
 
-public class McheckBox {
- private float x;
- private float y;
- private float w;
- private int team;
- private spawnUnit type;
- private triggers m;
+public class McheckBox extends  MunitBox {
  private unitDetect isFalse;
- private unitDetect nohasUnit;
- private unitRemove remove;
- private unitAdd setTag;
- public boolean safe;
  public McheckBox(triggers trg) {
-  this(trg.getPos(0, 0), trg.y, 1f, 0, new spawnUnit(unitType.other_dummyNonUnitWithTeam, 1), trg);
+  super(trg.getPos(0, 0), trg.y, 1f, 1f, 0, unitType.other_dummyNonUnitWithTeam, trg);
   safe = true;
  }
- public McheckBox(float x0, float y0, int t, triggers trg) {  
-  this(x0, y0, 20f, t, new spawnUnit(unitType.sea_lightSub, 1), trg);
-  type.offsetHeight = -8;
+ public McheckBox(float x0, float y0, int t, triggers trg) {
+  super(x0, y0, 14f, 14f, t, unitType.sea_lightSub, trg);
   safe = true;
  }
- public McheckBox(float x0, float y0, float w0, int t, spawnUnit unit, triggers trg) {
-  m = trg;
-  type = unit;
-  team = t;
-  x = x0;
-  y = y0;
-  w = w0;
- }
- public void set(unitObjects obj) throws IOException {
-  obj.add(x, y, team, type.type);
- }
- public unitAdd set() {
-  unitAdd add=setTag;
-  if (add == null) {
-   float w0=w * 0.5f;
-   setTag = add = new unitAdd(x + w0, y + w0, m, team, type);
-   add.resetActivationAfter = "1s";
-   if (!safe)add.linkOr(nohasUnit());
-  }
-  return add;
+ public McheckBox(float x0, float y0, float w0, int t, String type, triggers trg) {
+  super(x0, y0, w0, w0, t, type, trg);
  }
  public unitDetect False() {
   unitDetect de=isFalse;
   if (de == null) {
-   isFalse = de = add();
+   isFalse = de = detect();
    if (y >= 0) {
     de.onlyIdle = true;
    }
   }
   return de;
- }
- public unitDetect nohasUnit() {
-  unitDetect de=nohasUnit;
-  if (de == null) {
-   nohasUnit = de = add();
-   de.maxUnits = 0;
-  }
-  return de;
- }
- private unitDetect add() {
-  float w0=w;
-  unitDetect de = new unitDetect(x, y, w0, w0, m);
-  de.resetActivationAfter = "1s";
-  if (!safe) {
-   de.team = team;
-   de.unitType = type.type;
-  }
-  return de;
- }
- public unitRemove remove() {
-  unitRemove add=remove;
-  if (add == null) {
-   float w0=w;
-   remove = add = new unitRemove(x, y, w0, w0, m);
-   add.resetActivationAfter = "1s";
-   if (!safe)add.team = team;
-  }
-  return add;
  }
 }
