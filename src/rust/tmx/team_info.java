cc5f19bf46649ccdcab28c30477aa9e1e90f2f5c
n9tank <n9tank@163.com>
2024-12-19 15:54:35 +0800

    加下语法方便的东西

--- src/rust/tmx/team_info.java
+++ src/rust/tmx/team_info.java
@@ -1,17 +1,16 @@
 package rust.tmx;
 
 import java.util.concurrent.Callable;
-import java.io.IOException;
 
-public class team_info extends point implements Cloneable,Callable {
- private int t;
+public class team_info extends point implements Callable {
+ public int team=-3;
  private triggers m;
- public int credits;
- public boolean aiUse;
- public int ai;
- public int teams;
+ public long credits=Long.MAX_VALUE;
+ public boolean disabledAI;
+ public int ai=-3;
+ public int allyGroup=-3;
  public boolean aiAttack;
- public boolean aiBuild;
+ public boolean basicAI;
  public boolean shareFog;
  public static final int ai_veryEasy=-2;
  public static final int ai_easy=-1;
@@ -19,23 +18,22 @@ public class team_info extends point implements Cloneable,Callable {
  public static final int ai_hard=1;
  public static final int ai_veryHard=2;
  public static final int ai_impossible=3;
- public team_info(int id,triggers trg) {
-  super(0,0,trg);
-  t=id;
+ public team_info(int id, triggers trg) {
+  super(0, 0, trg);
+  team = id;
   trg.apply(this);
  }
  protected void before() throws Exception {
   super.before();
   triggers trg=m;
-  trg.append("team",t,0);
-  trg.append("disabledAI",aiUse);
-  trg.append("allyGroup",teams,0);
-  trg.append("credits",credits,0);
-  trg.append("shareFogWithAllies",shareFog);
-  trg.append("lockAiDifficulty",ai,0);
-  if(aiAttack)trg.append("ai","survival");
- }
- public team_info clone() throws CloneNotSupportedException{
- return (team_info)super.clone();
+  trg.append("team", team, -3);
+  trg.append("basicAI", basicAI);
+  trg.append("disabledAI", disabledAI);
+  trg.append("allyGroup", allyGroup, -3);
+  if (credits != Long.MAX_VALUE)
+   trg.append("credits", credits);
+  trg.append("shareFogWithAllies", shareFog);
+  trg.append("lockAiDifficulty", ai, -3);
+  if (aiAttack)trg.append("ai", "survival");
  }
 }
--- src/rust/tmx/unitRemove.java
+++ src/rust/tmx/unitRemove.java
@@ -1,12 +1,17 @@
 package rust.tmx;
 
 public class unitRemove extends basic {
+ public boolean onlyIfEmpty;
  public unitRemove(float x0, float y0, float w, float h, point g) {
   super(x0, y0, w, h, g);
-  unBox=false;
+  unBox = false;
  }
  public unitRemove(float x0, float y0, float w, float h, triggers triggers) {
   super(x0, y0, w, h, triggers);
-  unBox=false;
+  unBox = false;
+ }
+ protected void before() throws Exception {
+  super.before();
+  m.append("onlyIfEmpty", onlyIfEmpty);
  }
 }
